9bd6fd47f5641eec963d9f91549f348d8e213f02
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-15 23:54:07 -0400

    Updated and developed Employee class header file.

--- FactoryWorkersProgram/Employee.h
+++ FactoryWorkersProgram/Employee.h
@@ -1,8 +1,40 @@
 #ifndef EMPLOYEE_H
 #define EMPLOYEE_H
+#include <string>
 
 class Employee
 {
+private:
+    std::string name;
+    int number;
+
+    int yearHired;
+    int monthHired;
+    int dayHired;
+
+public:
+    Employee(std::string name, int num, int m, int d, int y);
+
+    void setName(std::string newName);
+    void setNumber(int newNum);
+    void setDateHired(int m, int d, int y);
+
+    // Accessor functions
+    inline std::string getName()
+    {
+        return name;
+    }
+    inline int getNumber()
+    {
+        return number;
+    }
+    inline std::string getDateHired()
+    {
+        std::string date = std::to_string(monthHired) + '/' + std::to_string(dayHired) + '/' + std::to_string(yearHired); // Formats date in mm/dd/yyyy
+        return date;
+    }
+
+    void printEmployee();
 
 };
 #endif // EMPLOYEE_H
--- FactoryWorkersProgram/main.cpp
+++ FactoryWorkersProgram/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include "Employee.h"
+#include "ProductionWorker.h"
+#include "ShiftSupervisor.h"
+#include "TeamLeader.h"
 
 using namespace std;
 
