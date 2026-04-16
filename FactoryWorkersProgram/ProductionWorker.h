42c5006560bdf9883f6da88fda87fb44b7d62981
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-16 16:49:48 -0400

    Defined ProductionWorker class with header file.

--- FactoryWorkersProgram/ProductionWorker.h
+++ FactoryWorkersProgram/ProductionWorker.h
@@ -1,8 +1,32 @@
 #ifndef PRODUCTIONWORKER_H
-#define PRODUCTIONWORKER_H]
+#define PRODUCTIONWORKER_H
+#include "Employee.h"
+#include <string>
 
-class ProductionWorker
+const int DAYSHIFT = 1;
+const int NIGHTSHIFT = 2;
+
+class ProductionWorker : public Employee
 {
+private:
+    int timeOfShift;
+    float payPerHour;
 
+public:
+    ProductionWorker();
+
+    inline int getShift()
+    {
+        return this->timeOfShift;
+    }
+    inline float getWage()
+    {
+        return this->payPerHour;
+    }
+
+    void setShift();
+    void setWage();
+
+    void printProductionWorker() const;
 };
 #endif // PRODUCTIONWORKER_H
