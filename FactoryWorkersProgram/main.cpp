0c49336e7bc8e6353f2b0a5e58d58a8920c5a522
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-17 00:31:18 -0400

    Tested Employee and ProductionWorker classes.
    Good results.

--- FactoryWorkersProgram/main.cpp
+++ FactoryWorkersProgram/main.cpp
@@ -8,6 +8,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Testing Employee Class:\n";
+
+    Employee bob("Robert", 999, 4, 9, 2026);
+
+    cout << bob.getName() << endl;
+    cout << bob.getNumber() << endl;
+    cout << bob.getDateHired() << endl;
+
+    bob.printEmployee(); // Should match with prior info.
+
+    cout << "Testing ProductionWorker Class:\n";
+
+    ProductionWorker sara("Sara", 6, 3, 14, 2025, DAYSHIFT, 15.00);
+    ProductionWorker james("James", 7, 2, 16, 2026, NIGHTSHIFT, 19.00);
+
+    sara.printProductionWorker();
+    james.printProductionWorker();
     return 0;
 }
