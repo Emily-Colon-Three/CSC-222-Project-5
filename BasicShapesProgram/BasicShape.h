26d5cda12a917705e2ddece3dbd8ebba76b614b7
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-18 00:29:41 -0400

    Defined Circle class with member declarations.

--- BasicShapesProgram/BasicShape.h
+++ BasicShapesProgram/BasicShape.h
@@ -1,3 +1,5 @@
+/* This class serves as an abstract base of more specific shape classes, not able to exist as an independent object. It contains the name and area common to all shapes, and member functions
+which address those values. These members will be utilized by all derived shape classes. */
 #ifndef BASICSHAPE_H
 #define BASICSHAPE_H
 #include <string>
@@ -21,6 +23,6 @@ public:
     void setArea(double newArea);
     void setName(std::string newName);
 
-    virtual void calcArea() = 0; // Pure virtual function
+    virtual void calcArea() = 0; // Pure virtual function; look at derived overrides to see actual properties and operations (though none have parameters nor a return)
 };
 #endif // BASICSHAPE_H
--- BasicShapesProgram/Circle.h
+++ BasicShapesProgram/Circle.h
@@ -1,9 +1,37 @@
+/* This class represents a circle, derived from base class BasicShape. It has circle-specific information added upon the existing data of the base, overriding its calcArea() function
+to work for a circle. */
 #ifndef CIRCLE_H
 #define CIRCLE_H
 #include "BasicShape.h"
+#include <string>
 
 class Circle : public BasicShape
 {
+private:
+    double xCenter;
+    double yCenter;
+    double radius;
 
+public:
+    Circle(double x, double y, double r, std::string n = "Circle");
+
+    // Getter functions
+    inline double getX()
+    {
+        return xCenter;
+    }
+    inline double getY()
+    {
+        return yCenter;
+    }
+    inline double getR()
+    {
+        return radius;
+    }
+
+    // Setter functions
+    void setX(double x);
+    void setY(double y);
+    void setR(double r);
 };
 #endif // CIRCLE_H
