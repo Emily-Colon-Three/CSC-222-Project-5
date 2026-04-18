4a383995fca17f06dc0ece63795acaac892d916c
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-18 17:29:02 -0400

    Defined Rectangle class, declared members.

--- BasicShapesProgram/Rectangle.cpp
+++ BasicShapesProgram/Rectangle.cpp
@@ -1,1 +1,22 @@
 #include "Rectangle.h"
+#include <string>
+
+Rectangle::Rectangle(double l, double w, std::string n)
+{
+
+}
+
+void Rectangle::setLength(double newLength)
+{
+
+}
+
+void Rectangle::setWidth(double newWidth)
+{
+
+}
+
+void Rectangle::calcArea()
+{
+
+}
--- BasicShapesProgram/Rectangle.h
+++ BasicShapesProgram/Rectangle.h
@@ -1,9 +1,33 @@
+/* This class represents a rectangle, derived from the class BasicShape. It has a length and width, its function to calculate the area of a Rectangle object overriding the
+pure virtual function of the base. */
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 #include "BasicShape.h"
+#include <string>
 
 class Rectangle : public BasicShape
 {
+private:
+    double length;
+    double width;
 
+public:
+    Rectangle(double l, double w, std::string  = "Rectangle");
+
+    // Getter functions
+    inline double getLength()
+    {
+        return this->length;
+    }
+    inline double getWidth()
+    {
+        returnb this->width;
+    }
+
+    // Setter functions
+    void setLength(double newLength);
+    void setWidth(double newWidth);
+
+    double calcArea();
 };
 #endif // RECTANGLE_H
