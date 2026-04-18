b5b960d97c06701f485374c2069a915d6764b283
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-18 00:14:58 -0400

    Defined and Developed BasicShape class.
    Relatively simple process, abstract and lays foundation for future classes.

--- BasicShapesProgram/BasicShape.cpp
+++ BasicShapesProgram/BasicShape.cpp
@@ -1,1 +1,12 @@
 #include "BasicShape.h"
+#include <string>
+
+void BasicShape::setArea(double newArea)
+{
+    this->area = newArea;
+}
+
+void BasicShape::setName(std::string newName)
+{
+    this->name = newName;
+}
--- BasicShapesProgram/BasicShape.h
+++ BasicShapesProgram/BasicShape.h
@@ -1,8 +1,26 @@
 #ifndef BASICSHAPE_H
 #define BASICSHAPE_H
+#include <string>
 
 class BasicShape
 {
+private:
+    double area;
+    std::string name;
 
+public:
+    inline double getArea()
+    {
+        return area;
+    }
+    inline std::string getName()
+    {
+        return name;
+    }
+
+    void setArea(double newArea);
+    void setName(std::string newName);
+
+    virtual void calcArea() = 0; // Pure virtual function
 };
 #endif // BASICSHAPE_H
