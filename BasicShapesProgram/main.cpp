b93b5fa32b3078dcdfc93d02519b231780c4c649
Emily <Emily.Olsen.Colonthree@outlook.com>
2026-04-18 20:57:21 -0400

    Tested Constructors; proper data printed.

--- BasicShapesProgram/main.cpp
+++ BasicShapesProgram/main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
+#include "BasicShape.h"
+#include "Circle.h"
+#include "Rectangle.h"
+#include "Square.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Construction and Polymorphism Testing:" << endl;
+
+    Rectangle rA(3, 5, "Rectangle A");
+    Rectangle rB(17, 2, "Rectangle B");
+    Circle cA(1, 12, 2.5, "Circle A");
+    Circle cB(-2, -7, 15, "Circle B");
+    Square sq(5);
+
+    BasicShape* collection[5] = {&rA, &rB, &cA, &cB, &sq}; // Creates dynamic array of basic shapes
+
+    // Loop to print out shape data to verify the success of object construction and polymorphic functions
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Name: " << collection[i]->getName() << endl;
+        cout << "Area: " << collection[i]->getArea() << endl;
+    }
+
     return 0;
 }
